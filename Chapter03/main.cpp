dc9295ca23ae4ad08b50f12bb4d78568c5648ff3
debarshee2004 <debarsheechakraborty.11d@gmail.com>
2024-03-03 05:52:44 +0530

    Chapter 03 Added

---
+++ Chapter03/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    /*
+        Errors:
+        1. Compile time errors
+        2. Runtime errors
+        3. Warnings
+    */
+
+    // std::cout << "Hello World" <<std::endl (Incorrect will give an error)
+    std::cout << "Hello World" <<std::endl; //(Correct)
+}
